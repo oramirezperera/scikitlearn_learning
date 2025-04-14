92b1b7617ef2bba1db12577e5031a1c8cbe241df
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-14 13:40:05 -0400

    Created the file to use the robust regressors

---
+++ robust.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+from sklearn.linear_model import (RANSACRegressor, HuberRegressor)
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+if __name__ == '__main__':
+    dataset = pd.read_csv('./data/felicidad_corrupt.csv')
+    print(dataset.head(5))
