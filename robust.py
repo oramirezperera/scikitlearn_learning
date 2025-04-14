5b3b99fe7e3fae148fad59bae51d1ed370ed63af
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-14 13:42:56 -0400

    Drop columns on the dataset

--- robust.py
+++ robust.py
@@ -8,3 +8,5 @@ from sklearn.metrics import mean_squared_error
 if __name__ == '__main__':
     dataset = pd.read_csv('./data/felicidad_corrupt.csv')
     print(dataset.head(5))
+
+    X = dataset.drop(['country', 'score'], axis=1)
