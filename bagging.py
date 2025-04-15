91bc6b0d1013077b2a71dc75119d9f3c1509aaac
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-15 09:40:46 -0400

    added the knn pred and bagging pred next I'm going to implement other classifiers

--- bagging.py
+++ bagging.py
@@ -15,3 +15,13 @@ if __name__ == '__main__':
     y = dt_heart['target']
 
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.35)
+
+    knn_class = KNeighborsClassifier().fit(X_train, y_train)
+    knn_pred = knn_class.predict(X_test)
+    print("="*64)
+    print(accuracy_score(knn_pred, y_test))
+
+    bag_class = BaggingClassifier(estimator=KNeighborsClassifier(), n_estimators=50).fit(X_train, y_train)
+    bag_pred = bag_class.predict(X_test)
+    print("="*64)
+    print(accuracy_score(bag_pred, y_test))
