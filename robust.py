eb6f713a8563b04cc3ee43f8f1c9db2dfa61a8a1
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-14 23:12:01 -0400

    changes to the y variable to solve a warning in the library

--- robust.py
+++ robust.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     print(dataset.head(5))
 
     X = dataset.drop(['country', 'score'], axis=1)
-    y = dataset[['score']]
+    y = dataset['score']
 
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=42)
 
