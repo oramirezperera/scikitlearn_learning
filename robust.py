6ba8d9402de8ed1b1293ae1da889b5e9b3c03828
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-14 22:45:34 -0400

    changes to robust.py added the estimators and printing in console the results

--- robust.py
+++ robust.py
@@ -10,3 +10,20 @@ if __name__ == '__main__':
     print(dataset.head(5))
 
     X = dataset.drop(['country', 'score'], axis=1)
+    y = dataset[['score']]
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=42)
+
+    estimators = {
+        'SVR' : SVR(gamma='auto', C=1.0, epsilon=0.1),
+        'RANSAC' : RANSACRegressor(),
+        'HUBER' : HuberRegressor(epsilon=1.35)
+    }
+
+    for name, estimator in estimators.items():
+        estimator.fit(X_train, y_train)
+        predictions = estimator.predict(X_test)
+
+        print("="*64)
+        print(name)
+        print("MSE: ", mean_squared_error(y_test, predictions))
