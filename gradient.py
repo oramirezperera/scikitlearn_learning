fcbf038e8dc7fffcbdc44cb2a5d7a52b77c9341a
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-15 10:05:02 -0400

    Created the basic structure of the file

--- gradient.py
+++ gradient.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+if __name__ == '__main__':
+
+    dt_heart = pd.read_csv('./data/heart.csv')
+    print(dt_heart['target'].describe())
