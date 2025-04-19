85bea7fa3d40cb97ba99615f71b8ca414195f3de
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-19 16:54:39 -0400

    added the kmeans fit and added the results into a new column named group

--- k_means.py
+++ k_means.py
@@ -5,3 +5,14 @@ from sklearn.cluster import MiniBatchKMeans
 if __name__ == '__main__':
     dataset = pd.read_csv('./data/candy.csv')
     print(dataset.head())
+
+    X = dataset.drop('competitorname', axis=1)
+
+    kmeans = MiniBatchKMeans(n_clusters=4, batch_size=8).fit(X)
+    print("Total centers: ", len(kmeans.cluster_centers_))
+    print("="*64)
+    print(kmeans.predict(X))
+
+    dataset['group'] = kmeans.predict(X)
+    print(dataset)
+
