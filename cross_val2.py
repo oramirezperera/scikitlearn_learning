64f0d8e4bf42e637730f953c1f9543b5de164c06
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-20 22:24:14 -0400

    In the new file we can see the mse and data in more detail

--- cross_val2.py
+++ cross_val2.py
@@ -3,7 +3,35 @@ import numpy as np
 
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import (cross_val_score, KFold)
+from sklearn.metrics import mean_squared_error
 
 if __name__ == '__main__':
     dataset = pd.read_csv('./data/felicidad.csv')
-    print(dataset.head())
+    
+    X = dataset.drop(['country', 'score'], axis=1)
+    y = dataset['score']
+
+    model = DecisionTreeRegressor()
+    score = cross_val_score(model, X,y, cv=3, scoring='neg_mean_squared_error')
+
+    print(score)
+    print(np.abs(np.mean(score)))
+
+    kf = KFold(n_splits=3, shuffle=True, random_state=42)
+
+    mse_values = []
+    for train, test in kf.split(dataset):
+        print(train)
+        print(test)
+
+        X_train = X.iloc[train]
+        y_train = y.iloc[train]
+        X_test = X.iloc[test]
+        y_test = y.iloc[test]
+
+        model = DecisionTreeRegressor().fit(X_train, y_train)
+        predict = model.predict(X_test)
+        mse_values.append(mean_squared_error(y_test, predict))
+
+    print("The three MSE were: ", mse_values)
+    print("Mean MSE was: ", np.mean(mse_values))
